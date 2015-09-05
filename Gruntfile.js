aab55d026175486ad2ef674a048b51c4f1ca25bc
Mathias Lindblom <ommatte@gmail.com>
2015-09-05 08:23:32 +0200

    Added less dependecy and automatic reading of less files, aka watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,27 @@ module.exports = function(grunt) {
     require("load-grunt-tasks")(grunt);
 
     var config = {
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "src/css/main.css": "src/less/main.less"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ["src/less/**/*.less"],
+                tasks: ["less"],
+                options: {
+                    nospawn: true
+                }
+            }
+        },
         browserify: {
             dev: {
                 src: ["src/engine.js"],
@@ -20,7 +41,10 @@ module.exports = function(grunt) {
 
     grunt.registerTask("build:dev", ["browserify:dev"]);
 
-    grunt.registerTask("build", ["build:dev"]);
+    grunt.registerTask("build", ["build:dev", "less"]);
 
-    grunt.registerTask("default", ["build"]);
+    grunt.registerTask("default", ["build", "watch"]);
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('assemble-less');
 };
