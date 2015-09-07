7e8e08e726064e7b53338effebc519806451736d
Erik Odenman <erik.odenman@gmail.com>
2015-09-07 21:20:22 +0200

    Big performance boost for play-area-renderer

--- src/js/game/play-area/play-area-renderer.js
+++ src/js/game/play-area/play-area-renderer.js
@@ -4,9 +4,18 @@ var canvasImageDataUtils = require("./canvas-image-data-utils.js");
 
 module.exports = function PlayAreaRenderer(gameState, playAreaContext, renderProperties) {
 
-    var mapBoundingBox = gameState.map.shape.boundingBox;
+    var UPDATE_GRANULARITY = 50; // Assuming map width and height is a multiple of the granularity
 
-    var image = playAreaContext.getImageData(0, 0, mapBoundingBox.width, mapBoundingBox.height);
+    var playAreaWidth = gameState.map.shape.boundingBox.width;
+    var playAreaHeight = gameState.map.shape.boundingBox.height;
+    var updateRows = playAreaHeight / UPDATE_GRANULARITY;
+    var updateCols = playAreaWidth / UPDATE_GRANULARITY;
+    var updatedSquare = new Array(updateRows * updateCols);
+    updatedSquare.forEach(function (value, index) {
+        updatedSquare[index] = false;
+    });
+
+    var image = playAreaContext.getImageData(0, 0, playAreaWidth, playAreaHeight);
 
     var render = function () {
         var updatedPixels = gameState.playAreaUpdateBuffer;
@@ -23,11 +32,22 @@ module.exports = function PlayAreaRenderer(gameState, playAreaContext, renderPro
                 var wormId = pixel.value;
                 color = COLORS[wormId];
             }
+            var row = Math.floor(pixel.index / playAreaWidth);
+            var col = pixel.index - row * playAreaWidth;
+            row = Math.floor(row / UPDATE_GRANULARITY);
+            col = Math.floor(col / UPDATE_GRANULARITY);
+            updatedSquare[row * updateCols + col] = true;
             canvasImageDataUtils.setColorByIndex(data, pixel.index, color);
-
         });
 
-        playAreaContext.putImageData(image, 0, 0);
+        for (var row = 0; row < updateRows; row++) {
+            for (var col = 0; col < updateCols; col++) {
+                if (updatedSquare[row * updateCols + col]) {
+                    updatedSquare[row * updateCols + col] = false;
+                    playAreaContext.putImageData(image, 0, 0, col * UPDATE_GRANULARITY, row * UPDATE_GRANULARITY, UPDATE_GRANULARITY, UPDATE_GRANULARITY);
+                }
+            }
+        }
     };
 
     return {
