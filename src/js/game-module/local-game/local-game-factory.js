6f336a36914ba691055c832b3ba8c5af31c6588c
Mathias Lindblom <ommatte@gmail.com>
2016-02-04 13:07:54 +0100

    Replay now working again. Seed is set

--- src/js/game-module/local-game/local-game-factory.js
+++ src/js/game-module/local-game/local-game-factory.js
@@ -31,7 +31,7 @@ module.exports = function LocalGameFactory() {
     function create(options) {
         var playerConfigs = options.playerConfigs;      //required
         var map = options.map || createDefaultMap();    //optional
-        var seed = options.seed;                        //optional
+        var seed = options.seed || Math.seedrandom();   //optional
 
         function createDefaultMap() {
             var sf = ShapeFactory();
