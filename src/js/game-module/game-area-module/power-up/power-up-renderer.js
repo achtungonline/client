8582f22984abb21a0178e7797dc708316a6e0187
Mathias Lindblom <ommatte@gmail.com>
2016-02-13 18:36:32 +0100

    Further refactored powerups

--- src/js/game-module/game-area-module/power-up/power-up-renderer.js
+++ src/js/game-module/game-area-module/power-up/power-up-renderer.js
@@ -4,17 +4,19 @@ module.exports = function PowerUpRenderer(gameState, powerUpContext, shapeRender
         powerUpContext.clearRect(0, 0, powerUpContext.canvas.width, powerUpContext.canvas.height);
         gameState.powerUps.forEach(function renderPowerUp(powerUp) {
             var color = "black";
-            if (powerUp.name === "Speed") {
+            if (powerUp.affects === "self") {
                 color = "green";
-            } else if (powerUp.name === "Slow") {
+            } else if (powerUp.affects === "others") {
                 color = "red";
+            } else if (powerUp.affects === "all") {
+                color = "blue";
             }
             shapeRenderer.render(powerUpContext, powerUp.shape, color);
             powerUpContext.font = "14px Arial";
             powerUpContext.textAlign = 'center';
-            powerUpContext.textBaseline="middle";
+            powerUpContext.textBaseline = "middle";
             powerUpContext.fillStyle = 'white';
-            powerUpContext.fillText(powerUp.name, powerUp.shape.centerX ,powerUp.shape.centerY);
+            powerUpContext.fillText(powerUp.name, powerUp.shape.centerX, powerUp.shape.centerY);
         });
     };
 
