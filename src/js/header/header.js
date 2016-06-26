82f9dc547c6d4278a632ce3d82f19879f1657996
Mathias Lindblom <ommatte@gmail.com>
2016-06-26 13:43:52 +0200

    fixed position facebook

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -3,13 +3,15 @@ module.exports = React.createClass({
     displayName: "Header",
     render: function () {
         return (
-            <div className="heading">
+            <div className="flex heading flex-space-between flex-align-end">
                 <h1>Achtung Online</h1>
-                <div
-                    className="fb-like"
-                    data-share="true"
-                    data-width="450"
-                    data-show-faces="true">
+                <div>
+                    <div
+                        className="fb-like"
+                        data-share="true"
+                        data-width="450"
+                        data-show-faces="true">
+                    </div>
                 </div>
             </div>
         )
