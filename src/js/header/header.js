3957c428eaa907ef04f21e0bff0e111a77e241a9
Mathias Lindblom <ommatte@gmail.com>
2016-06-26 14:08:16 +0200

    Added facebook page

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -13,6 +13,13 @@ module.exports = React.createClass({
                         data-show-faces="true">
                     </div>
                 </div>
+                <div class="fb-page" data-href="https://www.facebook.com/achtungonline/" data-tabs="timeline" data-height="70" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                    <blockquote cite="https://www.facebook.com/achtungonline/" class="fb-xfbml-parse-ignore">
+                        <a href="https://www.facebook.com/achtungonline/">
+                            Achtung Online
+                        </a>
+                    </blockquote>
+                </div>
             </div>
         )
     }
