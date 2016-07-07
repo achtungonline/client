22ef642d5eec083f51551fb599ce38ff3fad5d14
Mathias Lindblom <ommatte@gmail.com>
2016-07-07 11:30:21 +0200

    Added button to facebook page

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -6,21 +6,24 @@ module.exports = React.createClass({
             <div className="flex heading flex-space-between flex-align-end">
                 <h1>Achtung Online</h1>
                 <div>
-                    <div
-                        className="fb-like"
-                        data-share="true"
-                        data-width="450"
-                        data-show-faces="true">
-                    </div>
-                </div>
-                <div class="fb-page" data-href="https://www.facebook.com/achtungonline/" data-tabs="timeline" data-height="70" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                    <blockquote cite="https://www.facebook.com/achtungonline/" class="fb-xfbml-parse-ignore">
-                        <a href="https://www.facebook.com/achtungonline/">
-                            Achtung Online
-                        </a>
-                    </blockquote>
+                    <div className="fb-like" data-href="https://www.facebook.com/achtungonline" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
+                    <a href="https://www.facebook.com/achtungonline">
+                        <img className="m-l-2" style={{WebkitUserSelect: "none"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/F_icon.svg/1024px-F_icon.svg.png" width="30px"/>
+                    </a>
                 </div>
             </div>
         )
+    },
+    componentDidMount: function() {
+        console.log("Mounted");
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id))  {
+                return;
+            }
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=1197069846991445";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, "script", "facebook-jssdk"));
     }
 });
