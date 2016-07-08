9b61d6c56ce92ab1cdd2611407a17f4f65fff37b
Mathias Lindblom <ommatte@gmail.com>
2016-07-08 12:53:31 +0200

    Added under construction image in header

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -10,6 +10,10 @@ module.exports = React.createClass({
         return (
             <div className="flex heading flex-space-between flex-align-end">
                 <h1>Achtung Online</h1>
+                <div className="flex flex-align-center flex-self-center">
+                <img width="45px" src="https://upload.wikimedia.org/wikipedia/commons/2/27/W21-1a.svg"/>
+                    <small className="m-l-2">Website Under construction</small>
+                    </div>
                 <div>
                     <div className="fb-like" data-href="https://www.facebook.com/achtungonline" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
                     <a target="_blank" className="facebook-page-link m-l-2" href="https://www.facebook.com/achtungonline" dangerouslySetInnerHTML={{__html: svgTag}}/>
