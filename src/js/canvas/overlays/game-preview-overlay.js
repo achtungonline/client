0003c0fe5c0148c0a84044131c364167bd791820
Erik Odenman <erik.odenman@gmail.com>
2016-09-02 17:47:03 +0200

    Fixed the preview text font size and position for different maps

--- src/js/canvas/overlays/game-preview-overlay.js
+++ src/js/canvas/overlays/game-preview-overlay.js
@@ -3,18 +3,26 @@ var FADE_LOW_POINT = 0.2;
 
 var startTime = Date.now() / 1000;
 
-module.exports = function GamePreviewOverlay({ canvas }) {
+module.exports = function GamePreviewOverlay({ canvas, gameState }) {
     var context = canvas.getContext("2d");
 
     var render = function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
         var timeDiff = (Date.now()/1000 - startTime) % BLINK_DURATION;
         var a = timeDiff / BLINK_DURATION * Math.PI;
+        var fontSize = Math.max(canvas.width, canvas.height) / 25;
+        var offsetX = 19/20*canvas.width, offsetY = 1/20*canvas.height;
+        if (gameState.map.shape.type === "circle") {
+            offsetX = 99/100*canvas.width;
+            offsetY = 1/50*canvas.height;
+        }
+        offsetY += fontSize;
+
         context.globalAlpha = FADE_LOW_POINT + Math.sin(a) * (1 - FADE_LOW_POINT);
-        context.font = "20px bungee";
+        context.font = fontSize + "px bungee";
         context.fillStyle = "black";
-        context.textAlign = "center";
-        context.fillText("Preview", 7/8*canvas.width, 1/15*canvas.height);
+        context.textAlign = "right";
+        context.fillText("Preview", offsetX, offsetY);
     };
 
     return {
